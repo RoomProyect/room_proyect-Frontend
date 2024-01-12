48d38c744bc07ff4f5970ab59af22d9a9e4da91f
YamilCondori <yamil_condori_@outlook.com>
2024-01-12 20:42:06 -0300

    Working

---
+++ src/views/Detail/Detail.jsx
@@ -0,0 +1,37 @@
+import vivienda from '../../../../Pruebas/apartmentObj.js'
+
+const Detail = () =>{
+    return (
+        <>
+        <div className="w-full bg-[#F9F9F9] mb-10">
+            <div className="container max-w-full flex flex-row pt-6 pb-6 gap-10 lg:pl-24 md:pl-16 xl:gap-80 xl:pl-48 lg:gap-52 md:gap-40">
+                <h1 className="text-2xl" > TRAVEL </h1>
+
+                <input type="search" className="bg-black"/>
+
+                <button className="text-[#344E41] bg-black bg-opacity-25 p-1 pl-3 pr-3 rounded-[8px] text-1" >+ Agregar Hospedaje</button>
+            </div>
+        </div>
+        <div className="ml-14 mr-14 bg-black bg-opacity-5 flex flex-col justify-center items-center p-8 rounded-2x1 overflow-y-hidden">
+            <div className="flex justify-start items-start w-[100%]">
+                <button className="bg-[#F9F9F9]" > -- Back</button>
+            </div>
+            <div className="w-[100%] bg-red-600 p-10 flex">
+                <img src={vivienda.imagen} alt="house-image" className="w-[430px] h-[370px] rounded-3xl"/>
+                <div className='flex flex-col pl-4' >
+                    <h3 className='mb-3' >TITULO DEL POSTEO PARA LA CASA</h3>
+                    <p className='mb-3' > from {vivienda.precio} $$</p>
+                    <p> {vivienda.descripcion} </p>
+                </div>
+            </div>
+            <div className='w-[100%] mt-4'>
+                <h3 className='mb-4'>DETAILS</h3>
+
+                <p>{vivienda.descripcion}</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Detail;
