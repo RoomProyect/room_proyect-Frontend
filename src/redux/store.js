7821ed2512d9b2f47d61f3ce7df82eb3cb9ac243
HGomezCamargo <hernangc23@gmail.com>
2024-01-12 00:30:43 -0300

    comentario de prueba

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from './reducer';
-
+//Prueba de comentario
 
 const store = configureStore({
     // Automatically calls `combineReducers`
