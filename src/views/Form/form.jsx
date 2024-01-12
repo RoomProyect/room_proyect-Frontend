747e7a0ba22fe0dd74bb0e76f696cf15c0bf9552
HGomezCamargo <hernangc23@gmail.com>
2024-01-11 22:08:20 -0300

    avance form

--- src/views/Form/form.jsx
+++ src/views/Form/form.jsx
@@ -13,30 +13,28 @@ const form = () => {
         <h2>Formulario</h2>
         <form onSubmit = {handleSubmit(onSubmit)}>
             <label htmlFor="">Precio</label>
-            <input type="text" name="precio" id="precio" {...register("nombre")} />
+            <input type="text" name="precio" id="precio" {...register("precio")} />
 
             <label htmlFor="">Descripcion</label>
-            <input type="text" name="descripcion" id="descripcion" />
+            <input type="text" name="descripcion" id="descripcion" {...register("descripcion")}/>
 
             <label htmlFor="">Metros cuadrados de la Vivienda</label>
             <input type="text" name="mcCasa" id="mcCasa" />
             
             <label htmlFor="">Metros cuadrados del terreno</label>
-            <input type="text" name="mcTerreno" id="mcTerreno" />
+            <input type="text" name="mcTerreno" id="mcTerreno" {...register("mcTerreno")}/>
             
             <label htmlFor="">Ambientes</label>
-            <input type="text" name="ambientes" id="ambientes" />
+            <input type="text" name="ambientes" id="ambientes" {...register("ambientes")}/>
 
             <label htmlFor="">Baños</label>
-            <input type="text" name="baños" id="baños" />
+            <input type="text" name="baños" id="baños" {...register("baños")}/>
 
             <label htmlFor="">Cocheras</label>
-            <input type="text" name="cocheras" id="cocheras" />
+            <input type="text" name="cocheras" id="cocheras" {...register("cochera")}/>
 
             <label htmlFor=""></label>
             <input type="text" name="" id="" />
-
-
         </form>
     </div>
 }
