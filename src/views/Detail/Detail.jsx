81a401030c9d4d26564f3733b69fd5161b7acf05
sr.jimenez <danielmajimenez@gmail.com>
2024-01-17 03:07:12 -0400

    Creacion del zoom en imagenes

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,90 +1,159 @@
+import React, { useState } from "react";
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import NavBar from '../../componentes/navBar/NavBar.jsx';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import styles from './Detail.module.css';
 
-import ImgUno from '../../assets/cloudinary/fotosDetailPrueba/depto1.jpg'
-import ImgDos from '../../assets/cloudinary/fotosDetailPrueba/depto2.jpg'
-import ImgTres from '../../assets/cloudinary/fotosDetailPrueba/depto3.jpg'
+import ImgUno from '../../assets/cloudinary/fotosDetailPrueba/depto1.jpg';
+import ImgDos from '../../assets/cloudinary/fotosDetailPrueba/depto2.jpg';
+import ImgTres from '../../assets/cloudinary/fotosDetailPrueba/depto3.jpg';
 
 const Detail = () => {
     const { id } = useParams();
     const navigate = useNavigate();
     const vivienda = useSelector((state) => {
-    const response = state.counter.deptos.find((depto) => depto._id === id);
-    return response;
+        const response = state.counter.deptos.find((depto) => depto._id === id);
+        return response;
     });
+
+    const [isZoomed, setIsZoomed] = useState(false);
+
+    const handleZoom = () => {
+        setIsZoomed(!isZoomed);
+    };
 
     return (
         <>
-        {vivienda ? (
-            <>
             <div className={styles.navBarWrapper}>
                 <NavBar />
             </div>
             <div className={styles.container}>
                 <div className={styles.goBack}>
-                <button onClick={() => navigate(-1)}> {"< Back"}</button>
+                    <button onClick={() => navigate(-1)}> {"< Back"}</button>
                 </div>
                 <div className={styles.propertyDetails}>
-                    <img src={vivienda.img} alt="house-image" className={styles.propertyImage} />
-                <div className={styles.propertyInfo}>
-                    <h3 className={styles.propertyTitle}>{vivienda.titulo}</h3>
-                    <div className={styles.priceInfo}>
-                        <span className={styles.priceValue}>Por: {vivienda.precio}$</span>
-                    </div>
-                    <div className={styles.descripcion}>
-                        <span className={styles.descripcion}>{vivienda.descripcion}</span>
-                    </div>
-                    <div className={styles.tresImg}>
-                        <img src={ImgUno} alt="house-image" className={styles.tresImgIndividual} />
-                        <img src={ImgDos} alt="house-image" className={styles.tresImgIndividual} />
-                        <img src={ImgTres} alt="house-image" className={styles.tresImgIndividual} />
-                    </div>
-
-                </div>
+                    {isZoomed ? (
+                        <TransformWrapper
+                            initialScale={1}
+                            initialPositionX={200}
+                            initialPositionY={100}
+                        >
+                            {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                <TransformComponent>
+                                    <img
+                                        src={vivienda.img}
+                                        alt="house-image"
+                                        className={styles.propertyImage}
+                                    />
+                                    <div className="tools">
+                                        <button onClick={() => zoomIn()}>+</button>
+                                        <button onClick={() => zoomOut()}>-</button>
+                                        <button onClick={() => resetTransform()}>x</button>
+                                    </div>
+                                </TransformComponent>
+                            )}
+                        </TransformWrapper>
+                    ) : (
+                        <>
+                            <img
+                                src={vivienda.img}
+                                alt="house-image"
+                                className={styles.propertyImage}
+                                onClick={handleZoom}
+                            />
+                            <div className={styles.propertyInfo}>
+                                <h3 className={styles.propertyTitle}>{vivienda.titulo}</h3>
+                                <div className={styles.priceInfo}>
+                                    <span className={styles.priceValue}>Por: {vivienda.precio}$</span>
+                                </div>
+                                <div className={styles.descripcion}>
+                                    <span className={styles.descripcion}>{vivienda.descripcion}</span>
+                                </div>
+                                <div className={styles.tresImg}>
+                                    {/* Aplicar la funcionalidad de zoom a cada imagen individual */}
+                                    <TransformWrapper
+                                        initialScale={1}
+                                        initialPositionX={200}
+                                        initialPositionY={100}
+                                    >
+                                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                            <TransformComponent>
+                                                <img
+                                                    src={ImgUno}
+                                                    alt="house-image"
+                                                    className={styles.tresImgIndividual}
+                                                />
+                                                <div className="tools">
+                                                    <button onClick={() => zoomIn()}>+</button>
+                                                    <button onClick={() => zoomOut()}>-</button>
+                                                    <button onClick={() => resetTransform()}>x</button>
+                                                </div>
+                                            </TransformComponent>
+                                        )}
+                                    </TransformWrapper>
+                                    <TransformWrapper
+                                        initialScale={1}
+                                        initialPositionX={200}
+                                        initialPositionY={100}
+                                    >
+                                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                            <TransformComponent>
+                                                <img
+                                                    src={ImgDos}
+                                                    alt="house-image"
+                                                    className={styles.tresImgIndividual}
+                                                />
+                                                <div className="tools">
+                                                    <button onClick={() => zoomIn()}>+</button>
+                                                    <button onClick={() => zoomOut()}>-</button>
+                                                    <button onClick={() => resetTransform()}>x</button>
+                                                </div>
+                                            </TransformComponent>
+                                        )}
+                                    </TransformWrapper>
+                                    <TransformWrapper
+                                        initialScale={1}
+                                        initialPositionX={200}
+                                        initialPositionY={100}
+                                    >
+                                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                            <TransformComponent>
+                                                <img
+                                                    src={ImgTres}
+                                                    alt="house-image"
+                                                    className={styles.tresImgIndividual}
+                                                />
+                                                <div className="tools">
+                                                    <button onClick={() => zoomIn()}>+</button>
+                                                    <button onClick={() => zoomOut()}>-</button>
+                                                    <button onClick={() => resetTransform()}>x</button>
+                                                </div>
+                                            </TransformComponent>
+                                        )}
+                                    </TransformWrapper>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </div>
                 <div className={styles.additionalDetailsContainer}>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Ambientes: {vivienda.ambientes}</h6>
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Baños: {vivienda.baños}</h6>
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Cochera: {vivienda.cochera}</h6>
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Ciudad: {vivienda.ciudad}</h6>
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>mcTerreno: {vivienda.mcTerreno}</h6>
-                </div>
+                   
                 </div>
                 <div className={styles.buyButtonContainer}>
-                <button className={styles.buyButton}>
-                    Comprar
-                </button>
-                <button className={styles.buyButton}>
-                    Consultar
-                </button>
+                    <button className={styles.buyButton}>
+                        Comprar
+                    </button>
+                    <button className={styles.buyButton}>
+                        Consultar
+                    </button>
                 </div>
             </div>
-            </>
-        ) : (
-            <>
-            <div className={styles.navBarWrapper}>
-                <NavBar />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.goBack}>
-                <button onClick={() => navigate(-1)}> {"< Back"}</button>
-                </div>
-            </div>
-            </>
-        )}
         </>
     );
 };
 
 export default Detail;
+
+
+
