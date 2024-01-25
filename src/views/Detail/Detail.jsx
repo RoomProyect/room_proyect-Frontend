2bc4cbbba2f39a64244d64bec1112c5004cf8799
sr.jimenez <danielmajimenez@gmail.com>
2024-01-24 20:43:22 -0400

    Montado el mapa en detail solo falta modificar algo

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import NavBar from '../../componentes/navBar/NavBar.jsx';
 import styles from './Detail.module.css';
 import PayButton from '../../componentes/Stripe/PayButton.jsx';
 
-import ImgUno from '../../assets/cloudinary/fotosDetailPrueba/depto1.jpg'
-import ImgDos from '../../assets/cloudinary/fotosDetailPrueba/depto2.jpg'
-import ImgTres from '../../assets/cloudinary/fotosDetailPrueba/depto3.jpg'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+
+import ImgUno from '../../assets/cloudinary/fotosDetailPrueba/depto1.jpg';
+import ImgDos from '../../assets/cloudinary/fotosDetailPrueba/depto2.jpg';
+import ImgTres from '../../assets/cloudinary/fotosDetailPrueba/depto3.jpg';
 
 import cama from "../../assets/cloudinary/card/cama.svg";
 import casa from "../../assets/cloudinary/card/casa.png";
@@ -17,92 +21,101 @@ const Detail = () => {
     const { id } = useParams();
     const navigate = useNavigate();
     const vivienda = useSelector((state) => {
-    const response = state.counter.deptos.find((depto) => depto._id === id);
-    return response;
+        const response = state.counter.deptos.find((depto) => depto._id === id);
+        return response;
     });
 
+    const position = [-34.6118, -58.4173]; 
+    console.log({
+        position,
+        vivienda
+    })
     return (
-        <>
-        {vivienda ? (
-            <>
-            <div className={styles.navBarWrapper}>
-                <NavBar />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.goBack}>
-                <button onClick={() => navigate("/home")}> {"< Back"}</button>
-                </div>
-                <div className={styles.propertyDetails}>
-                    <img src={vivienda.img} alt="house-image" className={styles.propertyImage} />
-                <div className={styles.propertyInfo}>
-                    <h3 className={styles.propertyTitle}>{vivienda.titulo}</h3>
-                    <div className={styles.priceInfo}>
-                        <span className={styles.priceValue}>Por: {vivienda.precio}$</span>
+        <React.Fragment>
+            {vivienda ? (
+                <React.Fragment>
+                    <div className={styles.navBarWrapper}>
+                        <NavBar />
                     </div>
-                    <div className={styles.descripcion}>
-                        <span className={styles.descripcion}>{vivienda.descripcion}</span>
+                    <div className={styles.container}>
+                        <div className={styles.goBack}>
+                            <button onClick={() => navigate("/home")}> {"< Back"}</button>
+                        </div>
+                        <div className={styles.propertyDetails}>
+                            <img src={vivienda.img} alt="house-image" className={styles.propertyImage} />
+                            <div className={styles.propertyInfo}>
+                                <h3 className={styles.propertyTitle}>{vivienda.titulo}</h3>
+                                <div className={styles.priceInfo}>
+                                    <span className={styles.priceValue}>Por: {vivienda.precio}$</span>
+                                </div>
+                                <div className={styles.descripcion}>
+                                    <span className={styles.descripcion}>{vivienda.descripcion}</span>
+                                </div>
+                                <div className={styles.tresImg}>
+                                    <img src={ImgUno} alt="house-image" className={styles.tresImgIndividual} />
+                                    <img src={ImgDos} alt="house-image" className={styles.tresImgIndividual} />
+                                    <img src={ImgTres} alt="house-image" className={styles.tresImgIndividual} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.detailsContainer}>
+                            <div className={styles.detail}>
+                                <h6 className={styles.detailLabel}>Habitaciones: {vivienda.habitaciones}</h6>
+                                <img src={cama} alt="Cama" className={styles.icono} />
+                            </div>
+                            <div className={styles.detail}>
+                                <h6 className={styles.detailLabel}>Baños: {vivienda.baños}</h6>
+                                <img src={ducha} alt="Ducha" className={styles.icono} />
+                            </div>
+                            <div className={styles.detail}>
+                                <h6 className={styles.detailLabel}>Cochera: {vivienda.cochera}</h6>
+                            </div>
+                            <div className={styles.detail}>
+                                <h6 className={styles.detailLabel}>Ciudad: {vivienda.ciudad}</h6>
+                                <img src={ubi} alt="Ciudad" className={styles.icono} />
+                            </div>
+                            <div className={styles.detail}>
+                                <h6 className={styles.detailLabel}>mcTerreno: {vivienda.mcTerreno}</h6>
+                                <img src={casa} alt="Casa" className={styles.icono} />
+                            </div>
+                        </div>
+                        <div className={styles.buyButtonContainer}>
+                            <PayButton
+                                items={vivienda}
+                            />
+                            <button className={styles.buyButton}>
+                                Consultar
+                            </button>
+                        </div>
+                        <MapContainer center={position} zoom={13} scrollWheelZoom={false} className={styles.mapContainer}>
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            className={styles.tileLayerStyle}/>
+                            <Marker position={position}>
+                                <Popup className={styles.popupStyle}>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
                     </div>
-                    <div className={styles.tresImg}>
-                        <img src={ImgUno} alt="house-image" className={styles.tresImgIndividual} />
-                        <img src={ImgDos} alt="house-image" className={styles.tresImgIndividual} />
-                        <img src={ImgTres} alt="house-image" className={styles.tresImgIndividual} />
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <div className={styles.navBarWrapper}>
+                        <NavBar />
                     </div>
-
-                </div>
-                </div>
-                <div className={styles.detailsContainer}>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Habitaciones: {vivienda.habitaciones}</h6>
-                    <img src={cama} alt="Cama" className={styles.icono} />
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Baños: {vivienda.baños}</h6>
-                    <img src={ducha} alt="Ducha" className={styles.icono} />
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Cochera: {vivienda.cochera}</h6>
-
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>Ciudad: {vivienda.ciudad}</h6>
-                    <img src={ubi} alt="Ciudad" className={styles.icono}/>
-                </div>
-                <div className={styles.detail}>
-                    <h6 className={styles.detailLabel}>mcTerreno: {vivienda.mcTerreno}</h6>
-                    <img src={casa} alt="Casa" className={styles.icono} />
-                </div>
-            </div>
-                <div className={styles.buyButtonContainer}>
-                <PayButton
-                        items={vivienda}
-                    />
-                <button className={styles.buyButton}>
-                    Consultar
-                </button>
-                </div>
-            </div>
-            </>
-        ) : (
-            <>
-            <div className={styles.navBarWrapper}>
-                <NavBar />
-            </div>
-            <div className={styles.container}>
-                <div className={styles.goBack}>
-                <button onClick={() => navigate("/home")}> {"< Back"}</button>
-                </div>
-            </div>
-            </>
-        )}
-        </>
+                    <div className={styles.container}>
+                        <div className={styles.goBack}>
+                            <button onClick={() => navigate("/home")}> {"< Back"}</button>
+                        </div>
+                    </div>
+                </React.Fragment>
+            )}
+        </React.Fragment>
     );
 };
 
 export default Detail;
-
-
-
-
 
 
 
