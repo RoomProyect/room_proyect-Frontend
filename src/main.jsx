5eb8f1df241ed7093e12fab138e3305604772aeb
JMRodriguezV98 <mriciosama@gmail.com>
2024-01-31 19:31:14 -0500

    fix hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
-import { HashRouter } from 'react-router-dom'
 
 axios.defaults.baseURL = 'https://room-project-backend.onrender.com';
 
